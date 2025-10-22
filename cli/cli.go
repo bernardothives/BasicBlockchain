e7efbf202871cbdf45f6810af74475663b23d135
bernardothives <bernardoalvesthives@gmail.com>
2025-10-22 20:47:33 -0300

    cli

---
+++ cli/cli.go
@@ -0,0 +1,87 @@
+package cli
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/bernardothives/BasicBlockchain/blockchain"
+)
+
+type CLI struct {
+	bc *blockchain.Blockchain
+}
+
+func NewCLI(bc *blockchain.Blockchain) *CLI {
+	return &CLI{bc}
+}
+
+func (cli *CLI) Run() {
+	if len(os.Args) < 2 {
+		cli.printUsage()
+		os.Exit(1)
+	}
+
+	addBlockCmd := flag.NewFlagSet("addblock", flag.ExitOnError)
+	printChainCmd := flag.NewFlagSet("printchain", flag.ExitOnError)
+
+	addBlockData := addBlockCmd.String("data", "", "Block data")
+
+	switch os.Args[1] {
+	case "addblock":
+		err := addBlockCmd.Parse(os.Args[2:])
+		if err != nil {
+			log.Panic(err)
+		}
+	case "printchain":
+		err := printChainCmd.Parse(os.Args[2:])
+		if err != nil {
+			log.Panic(err)
+		}
+	default:
+		cli.printUsage()
+		os.Exit(1)
+	}
+
+	if addBlockCmd.Parsed() {
+		if *addBlockData == "" {
+			addBlockCmd.Usage()
+			os.Exit(1)
+		}
+		cli.addBlock(*addBlockData)
+	}
+
+	if printChainCmd.Parsed() {
+		cli.printChain()
+	}
+}
+
+func (cli *CLI) addBlock(data string) {
+	cli.bc.AddBlock(data)
+	fmt.Println("Success!")
+}
+
+func (cli *CLI) printChain() {
+	bci := cli.bc.Iterator()
+
+	for {
+		block := bci.Next()
+
+		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+		pow := blockchain.NewProofOfWork(block)
+		fmt.Printf("PoW: %v\n\n", pow.Validate())
+
+		if len(block.PrevBlockHash) == 0 {
+			break
+		}
+	}
+}
+
+func (cli *CLI) printUsage() {
+	fmt.Println("Usage:")
+	fmt.Println("  addblock -data BLOCK_DATA - add a block to the blockchain")
+	fmt.Println("  printchain - print all the blocks of the blockchain")
+}
--- main.go
+++ main.go
@@ -1,41 +1,21 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"strconv"
+
+	"github.com/bernardothives/BasicBlockchain/cli"
 
 	"github.com/bernardothives/BasicBlockchain/blockchain"
 )
 
 func main() {
 	bc := blockchain.NewBlockchain()
-
 	defer func() {
 		if err := bc.Db.Close(); err != nil {
 			log.Panic(err)
 		}
 	}()
 
-	// bc.AddBlock("Enviando 1 BTC para o João")
-	// bc.AddBlock("Enviando 2.5 BTC para a Maria")
-
-	bci := bc.Iterator()
-
-	fmt.Println("----- IMPRIMINDO BLOCKCHAIN -----")
-	for {
-		block := bci.Next()
-
-		fmt.Printf("Hash Anterior: %x\n", block.PrevBlockHash)
-		fmt.Printf("Dados: %s\n", block.Data)
-		fmt.Printf("Hash Atual: %x\n", block.Hash)
-
-		pow := blockchain.NewProofOfWork(block)
-		fmt.Printf("PoW Válido: %s\n", strconv.FormatBool(pow.Validate()))
-		fmt.Println("------------------------------------")
-
-		if len(block.PrevBlockHash) == 0 {
-			break
-		}
-	}
+	cli := cli.NewCLI(bc)
+	cli.Run()
 }
